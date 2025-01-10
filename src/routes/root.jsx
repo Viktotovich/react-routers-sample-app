b37adbe0c9838807cefda843643255b3d2141ffe
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-11 00:23:47 +0400

    Implemented search with help of the tutorial/docs

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,27 +7,33 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../data/contacts";
+import { useEffect } from "react";
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
@@ -72,9 +78,11 @@ export default function Root() {
   );
 }
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export async function action() {
