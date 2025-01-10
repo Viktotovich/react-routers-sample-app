0b7f7a20bb7c78c7d62ec0bab7d29c3554768c15
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-10 19:55:55 +0400

    Project starting point, template reformated to support the tutorial requirements

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//demo router setup
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
-
-const router = createBrowserRouter(routes);
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <p>Hello World</p>
   </StrictMode>
 );
